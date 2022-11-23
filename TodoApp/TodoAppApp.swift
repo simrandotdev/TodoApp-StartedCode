ad2fce875fd2ec93aa3814bd5a4089fe8fae580d
Simran Preet Singh <simran5590@gmail.com>
2022-11-22 20:05:43 -0800

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by Simran Preet Narang on 2022-11-22.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
